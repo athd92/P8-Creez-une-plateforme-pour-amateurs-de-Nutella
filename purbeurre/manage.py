b95130ab32fbb8e96147995bb0bb974d5a14c509
athd92 <antoinecours@gmx.fr>
2019-12-30 14:13:13 +0100

    purdbeurre repo modified

--- purbeurre/manage.py
+++ purbeurre/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pubeurre.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'purbeurre.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
