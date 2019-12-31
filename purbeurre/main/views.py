de20ac79fc6b48cbcb740d456285315deffa9ee2
athd92 <antoinecours@gmx.fr>
2019-12-31 12:09:23 +0100

    jquery and bootstrap installed

--- purbeurre/main/views.py
+++ purbeurre/main/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+
 def homepage(request):
-    return HttpResponse("Homepage")
+    test = '5'
+    return render(request = request,
+                  template_name='main/homepage.html',
+                  context = {"tutorials":test})
--- purbeurre/purbeurre/settings.py
+++ purbeurre/purbeurre/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main.apps.MainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'purbeurre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'purbeurredb',
+        'USER': 'guest',
+        'PASSWORD': 'password1234',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
