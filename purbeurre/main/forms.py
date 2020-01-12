931fb4713c8399444fd8036a94df64631980bacc
athd92 <antoinecours@gmx.fr>
2020-01-12 12:32:00 +0100

    update

--- purbeurre/main/forms.py
+++ purbeurre/main/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+
 class UserFormWithEmail(UserCreationForm):
     email = forms.EmailField(required=True)
 
@@ -10,7 +11,7 @@ class UserFormWithEmail(UserCreationForm):
         fields = ("username", "email", "password1", "password2")
 
     def save(self, commit=True):
-        user = super(NewUserForm, self).save(commit=False)
+        user = super(UserFormWithEmail, self).save(commit=False)
         user.email = self.cleaned_data["email"]
         if commit:
             user.save()
